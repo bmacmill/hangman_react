cfbffd52f5d92d6f9f95ef8790b2064be5408ebe
bmacmill <bnobile@gmail.com>
2024-12-26 10:27:20 -0500

    set up header component
    setting up and styling header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+// import "../App.css"
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Assembly endgame</h1>
+            <p>Guess the word within 8 attempts to keep the
+                programming world safe from Assembly!</p>
+        </header>
+    )
+}
