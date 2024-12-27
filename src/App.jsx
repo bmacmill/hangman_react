e17731486dc3437992e7cea19c1de0b5ef89421c
bmacmill <bnobile@gmail.com>
2024-12-26 19:56:46 -0500

    make game status section
    hard code and style the game status section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import './App.css'
 import Header from "./components/Header"
+import Status from "./components/Status"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Header />
+      <Status />
     </>
   )
 }
---
+++ src/components/Status.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+export default function Status() {
+    return (
+        <section className="Status">
+            <h2>You Win</h2>
+            <p>Well done 🎉!</p>
+        </section>
+    )
+}
