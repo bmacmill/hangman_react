ae86e20b5f60daf0616936a1cb3ee185fc1f83bb
bmacmill <bnobile@gmail.com>
2025-01-08 11:09:34 -0500

    status message update
    getting status message to work locally for now. it's ugly but can refactor later.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 what I learned
 -make paragraph a max width if you want it to wrap
 -margin top and bottom use margin-block: XXpx;
-
+-lifting state up when necessary; heard mentioned many times but never had to do it. it's a little confusing but nothing to be 'scared' about!
 -using the npm package clsx to render complex/compund class names
 -making derived state out of existing state for keyboard component, much easier than making 2 more state instances!
 -nested ternary as in the status component, it's ugly but works.
--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,16 +1,36 @@
 import React from "react"
-
+import { getFarewellText } from "../../utils"
 // same derived state as from keyboard component, should I make sate?
+import { languages } from "../../languages.js"
+
 export default function Status({ guessedLetters, currentWord }) {
+    console.log(getFarewellText(languages[0].name))
+    
     const wrongLetters = guessedLetters.filter((letter) => !currentWord.includes(letter))
     const gameWon = currentWord.split("").every((letter) => guessedLetters.includes(letter))
 
     const isGameOver = wrongLetters.length === 8 || gameWon
-
+    //find if last guess is correct or not?
+    const lastGuess = guessedLetters[guessedLetters.length - 1]
+    const lastGuessCorrect = currentWord.includes(lastGuess)
+    console.log(lastGuessCorrect)
+    //get last guess of guessed letters array
+    //see if last guess is in the currentWord
+    const msg = ["nope", "nice try", "nyet", "un nuh", "ah doy"]
+    const rand = Math.floor(Math.random() * msg.length)
+    console.log(rand)
+    if (!isGameOver && lastGuess && !lastGuessCorrect) {
+        return <section className="Status wrongMsg" style={{ opacity: 1 }}>
+            <h2>MSG</h2>
+            <p>{msg[rand]}</p>
+        </section>
+    }
     return (
+
+
         isGameOver ? (
             gameWon ? (
-                <section className="Status" style={{ opacity: 1 }}>
+                <section className="Status Win" style={{ opacity: 1 }}>
                     <h2>You Win</h2>
                     <p>Well done 🎉!</p>
                 </section>
