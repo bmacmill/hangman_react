7222b61e958bd285828d34a954c2b0efa28de76c
bmacmill <bnobile@gmail.com>
2025-01-21 16:53:07 -0500

    a11y fixes to word component
    add read-out of guess for screen readers only

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -3,7 +3,8 @@ import React from "react"
 
 export default function Word({ currentWord, guessedLetters }) {
     //const [currentWord, setCurrentWord] = React.useState("react")
-
+    //vfor a11y read out of guesses
+    const lastGuess = guessedLetters[guessedLetters.length - 1]
     const letterElements = currentWord.split("").map((letter, index) => {
         return (
             <span key={self.crypto.randomUUID()}> {guessedLetters.includes(letter) ? letter : ""}</span >
@@ -17,8 +18,14 @@ export default function Word({ currentWord, guessedLetters }) {
             {letterElements}
         </section>
 
-{/* screen reader only secition period after letter adds a little pause*/}
+{/* screen reader only section hidden from view; note: after letter adds a little pause*/}
         <section className="sr-only" aria-live="polite" status="role">
+        {/* adding guessed letter read out */}
+            <p>{currentWord.includes(lastGuess) ? 
+                        `Correct! The letter ${lastGuess} is in the word.` : 
+                        `Sorry, the letter ${lastGuess} is not in the word.`
+                    }
+            </p>
              <p>current word: {currentWord.split("").map((letter) => guessedLetters.includes(letter) ? letter + "." : "blank")}</p>
             </section>
         </>
